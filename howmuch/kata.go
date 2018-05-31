f8017353009be58b43950fef2f9907c6f474355a
fernandoocampo <fernando.ocampo@ravencall.io>
2018-05-30 21:11:33 -0500

    Added skeleton for howmuch kata

---
+++ howmuch/kata.go
@@ -0,0 +1,5 @@
+package howmuch
+
+func HowMuch(m int, n int) [][3]string {
+	return nil
+}
--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
+func reverse(s []int) {
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
 func main() {
-	fmt.Println(strings.Count("indivisibility", "i"))
+	s := []int{0, 1, 2, 3, 4, 5}
+	reverse(s[:2])
+	fmt.Printf("%v\n", s)
+	reverse(s[2:])
+	fmt.Printf("%v\n", s)
+	reverse(s)
+	fmt.Printf("%v\n", s)
 }
